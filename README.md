6ec15be592cabe4983bb976374fd479c79c276b8
Federico Bonfiglio <federico.bonfiglio@protonmail.ch>
2022-05-17 19:44:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Hypothesis:
+
+meme diffusion follows a power law.
+
+Methodology:
+Scrape Reddit for new memes and follow their evolution for an appropriate period of time (which is...?)
+Try to fit it to a Power Law distribution for each memes.
+
+TODO find out:
+- appropriate length of time to monitor memes
+- appropriate number of memes
+- appropriate poll time for new stats about tracked memes
+
+Data scraping:
+
+1. query Reddit for new media posts in r/dankmemes
+2. save n of them for tracking
+3. every k minutes, retrieve those n posts again, query each of them for
+    - likes
+    - dislikes
+    - comments
+    - upvote ratio
+    - score
+    - awards 
+4. append to csv along with timestamp of API call
+
+Data analysis:
+1. inspect data with plots and various statistics
+2. try fitting with power law
+
+TODO investigate if Reddit API has better stuff for my goals
---
+++ scrape.py
@@ -0,0 +1,41 @@
+from datetime import datetime as dt
+import threading
+import time
+
+import requests
+
+def appendRow(file, data, timestamp):
+    name = str(data['name'])
+    upvotes = str(data['ups'])
+    downvotes = str(data['downs'])
+    upvote_ratio = str(data['upvote_ratio'])
+    score = str(data['score'])
+    file.write(f'{timestamp};{name};{upvotes};{downvotes};{upvote_ratio};{score}\n')
+
+
+
+def main():
+    limit = 10
+    reddit_time = 'hour' # hour, year, etc...
+
+    headers = {'User-agent': 'Firefox'}
+
+    # First get which posts to track
+    res = requests.get(f'https://www.reddit.com/r/dankmemes/new.json?limit={limit}&t={reddit_time}', headers=headers).json()
+
+    tracked = [ p['data']['name']  for p in res['data']['children'] ]
+
+    timestamp = str(dt.now()).replace(' ', '_')
+    with open(f'data_{timestamp}.csv', 'w') as file:
+        file.write('time;name;upvotes;downvotes;upvote_ratio;score\n')
+        while True:
+            timestamp = str(dt.now())
+            print(f"{timestamp} | Collecting {tracked}")
+            for post in tracked:
+                res = requests.get(f'https://www.reddit.com/by_id/{post}.json', headers=headers)
+                child = res.json()['data']['children'][0]
+                appendRow(file, child['data'], timestamp);
+            time.sleep(1)
+
+if __name__=='__main__':
+    main()
